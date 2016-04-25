af7d7de8f1ddbb1529aefde95420c7ac7e3b2a2e
jbudrow <jbudrowd3@yahoo.com>
2016-04-25 12:21:56 -0400

    create user controller, along with model validations, auth-token gen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  def current_user
+    token = request.headers['Auth-token']
+    if token
+      User.find_by auth_token: token
+    end
+  end
+
+  def authenticate!
+    unless current_user
+      render json: { errors: 'Please log in first' }
+                     status: :unauthorized
+    end
+  end
 end
