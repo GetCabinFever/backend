91b6df07d25dc6fe57c088bf269da084cb6bc415
tallkeith <tallestkeith@gmail.com>
2016-04-26 17:18:29 -0400

    added amenities and safety to residence create method

--- app/controllers/residences_controller.rb
+++ app/controllers/residences_controller.rb
@@ -3,6 +3,8 @@ class ResidencesController < ApplicationController
 
   def create
     @residence = Residence.new(residence_params)
+    @residence.amenities = Amenities.new(amenities_params)
+    @residence.safety = Safety.new(safeties_params)
     if @residence.save
       render 'create.json.jbuilder',
       status: :success
@@ -24,8 +26,24 @@ class ResidencesController < ApplicationController
 
   private
   def residence_params
-    params.permit(:title_of_page, :address, :city, :state, :zip, :property_type, :contact_host_email, :base_price,
-    :bedrooms, :bathrooms, :beds, :description_of_listing, :house_rules, :the_space, :guest_access,
-    :interaction_with_guests, :the_area, :other_things_to_note, :check_in, :check_out, :image)
+    params.permit(:title_of_page, :address, :city, :state, :zip, 
+                  :property_type, :contact_host_email, :base_price, :bedrooms, 
+                  :bathrooms, :beds, :description_of_listing, :house_rules, 
+                  :the_space, :guest_access, :interaction_with_guests, :the_area, 
+                  :other_things_to_note, :check_in, :check_out, :image, :booking_url)
   end
+
+  def amenities_params
+    params.permit(:kitchen, :internet, :tv, :essentials, :shower_essentials, 
+                  :heating, :airconditioning, :washer, :dryer, :dishwasher, 
+                  :free_parking, :cable, :satellite, :breakfast, :pets, 
+                  :kid_friendly, :events, :smoking, :wheelchair_accessible, 
+                  :elevator, :fireplace, :intercom, :doorman, :pool, :hottub, 
+                  :gym, :hangers, :iron, :hair_dryer, :workstation, :billiards)
+  end
+
+  def safeties_params
+    params.permit(:smoke, :co2, :first_aid, :safety_card, :extinguisher, :fire_alarm, :gas_shutoff, :generator)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   post "login", to: "registrations#login"
   post "register", to: "registrations#create"
 
+  post "create-listing", to: "residences#create"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
