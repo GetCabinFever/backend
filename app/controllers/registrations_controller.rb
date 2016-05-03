7007bee25f8a60e938349f1a710ac953810acf35
jbudrow <jbudrowd3@yahoo.com>
2016-05-03 13:48:37 -0400

    Functional action mailer

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,8 +4,7 @@ class RegistrationsController < ApplicationController
     @user = User.new(user_params)
     @user.ensure_auth_token
     if @user.save
-      mail = UserMailer.welcome(@user)
-      mail.deliver_now
+      UserMailer.welcome_email(@user).deliver_now
       render "create.json.jbuilder", status: :ok
     else
       render json: { errors: @user.errors.full_messages },
