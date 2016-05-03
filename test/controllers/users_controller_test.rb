6f6dff0ad9559e0e02aeab92a6e4ddd7a3d78424
jbudrow <jbudrowd3@yahoo.com>
2016-05-03 12:18:11 -0400

    Update users test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,17 +4,23 @@ class UsersControllerTest < ActionController::TestCase
 
   setup do
     @user = users(:tycho)
+    request.headers["X-Auth-Token"] = @user.auth_token
   end
 
-  teardown do
-    @user.destroy!
-  end
-
-  # test 'should successfully update user' do
-  #   updated_email = 'tycho@domain.com'
-  #
-  #   patch :update, id: @user.id, user: { email: updated_email }
-  #   assert_equal updated_email, @user.email
-  #   assert_response 200
+  # test "users can't update other users" do
+  #   patch :update, id: users(:brit).id, email: "ur-hacked@haha.com"
+  #   assert_response :unauthorized
   # end
+  #
+  # test "fake users cannot be updated" do
+  #   patch :update, id: 9999999, email: "bull@shit.com"
+  #   assert_response :not_found
+  # end
+
+  test "only logged in users can update themselves" do
+    patch :update, id: @user.id, email: "foo@bar.com"
+    assert_not_equal @user.email, User.find(@user.id).email
+    assert_response :success
+    assert_template "update.json.jbuilder"
+  end
 end
